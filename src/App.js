7cae4ad11456113b76dc7ad72576e3c4973470ee
Elena Bravo <hey@elenabravo.com>
2020-07-24 15:45:22 +0200

    add components, functionality, styles and propTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 // Styles
 import styles from './App.module.sass'
@@ -8,22 +8,86 @@ import HoneyCombIcon from './components/Icons/Honeycomb'
 import PinpadButton from './components/Button/PinpadButton.js'
 
 const App = () => {
+  const correctPin = '4747' /* Hardcoded pin. To be fetched from an API */
   const pinNumbers = [...Array(10).keys()].reverse() /* Generate pin pad numbers */
-  const placeholder = 'Enter pin'
+  const placeholder = 'Enter pin' /* Pin pad display placeholder text */
+  const maxAttempts = 2 /* Max number of incorrect attempts */
+  const resetTime = 1200 /* Reset time when pin pad status is ok or error */
+  const lockedTime = 30000 /* Reset time when pin pad status is locked */
+
+  const [pin, setPin] = useState('')
+  const [status, setStatus] = useState()
+  const [disabled, setDisabled] = useState(false)
+  const attempts = useRef(0)
+
+  const handlePin = (number) => {
+    if (pin.length < correctPin.length) {
+      setPin(pin + number)
+    }
+  }
+
+  const handleSetAttempts = (attempt) => {
+    attempts.current = attempt
+  }
+
+  const handleReset = async (time) => {
+    await setTimeout(() => {
+      setPin('')
+      setStatus()
+      setDisabled(false)
+    }, time)
+  }
+
+  // Set screen text as pin if entered. Otherwise display the placeholder
+  const displayText = pin.length > 0
+    ? pin.split('').map((number, index, array) => (index + 1) === pin.length ? number : '*').join('')
+    : placeholder
+
+  useEffect(() => {
+    // Check status once all pin numbers have been entered
+    if (pin.length === correctPin.length) {
+      const { current: currentAttempts } = attempts
+      let currentStatus, attemptsToUpdate,
+        timeOut = resetTime
+
+      if (pin === correctPin) {
+        currentStatus = 'ok'
+        attemptsToUpdate = 0
+
+      } else if (maxAttempts === currentAttempts) {
+        currentStatus = 'locked'
+        attemptsToUpdate = 0
+        timeOut = lockedTime
+
+      } else {
+        currentStatus = 'error'
+        attemptsToUpdate = currentAttempts + 1
+      }
+
+      handleSetAttempts(attemptsToUpdate)
+      setStatus(currentStatus)
+      setDisabled(true)
+      handleReset(timeOut)
+    }
+
+  }, [pin])
 
   return (
     <div className={styles.pinpad__wrapper}>
-      <HoneyCombIcon />
+      <HoneyCombIcon className={status} />
       <div className={styles.pinpad}>
-        <div className={styles.pinpad__screen}>
-          <span className={styles.pinpad__screen__text}>{placeholder}</span>
+        <div className={styles.pinpad__display}>
+          <span className={styles.pinpad__display__text}>
+            {status || displayText}
+          </span>
         </div>
         <div className={styles.pinpad__btnGroup}>
           {pinNumbers.map(pinNumber => (
             <PinpadButton
               key={pinNumber}
-              data={pinNumber}
-              action={() => console.log(pinNumber)}
+              value={pinNumber.toString()}
+              disabled={disabled}
+              action={() => handlePin(pinNumber)}
             />
           ))}
         </div>
--- src/components/Button/PinPadButton.js
+++ src/components/Button/PinPadButton.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // Styles
 import styles from './PinpadButton.module.sass'
 
-const PinpadButton = ({ data, action }) => {
+const PinpadButton = ({ value, action, disabled }) => {
   return (
-    <button className={styles.pinpadButton} type="button" onClick={action}>
-      <span className={styles.pinpadButton__number}>{data}</span>
+    <button
+      className={`${styles.pinpadButton} ${disabled ? styles.disabled : ''}`}
+      type="button"
+      disabled={disabled}
+      onClick={action}
+    >
+      <span className={styles.pinpadButton__number}>{value}</span>
     </button>
   )
+}
+
+PinpadButton.defaultProps = {
+  disabled: false
+}
+
+PinpadButton.propTypes = {
+  value: PropTypes.string.isRequired,
+  action: PropTypes.func.isRequired,
+  disabled: PropTypes.bool
 }
 
 export default PinpadButton
